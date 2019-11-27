d71f4d2f0483f052d492cb5637b88bfec071eea5
Nir Adar <niromika88@gmail.com>
2019-11-27 16:41:42 +0200

    first commit

---
+++ precache-manifest.cf7c2268b5399303a53dfe5b2a70ff61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2339ebac79842e1e56635cae9dd72f1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dfc2d217d7743e34286",
+    "url": "/static/css/main.c1d2f96b.chunk.css"
+  },
+  {
+    "revision": "a7ba8016851e91548f7e",
+    "url": "/static/js/2.5396072c.chunk.js"
+  },
+  {
+    "revision": "0dfc2d217d7743e34286",
+    "url": "/static/js/main.5b5ad005.chunk.js"
+  },
+  {
+    "revision": "68cd43422941e67a157b",
+    "url": "/static/js/runtime-main.dda48577.js"
+  }
+]);
